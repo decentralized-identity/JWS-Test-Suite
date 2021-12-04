fe38b8225b768147c159294b644e50cd58b8f3d5
Markus Sabadello <markus@danubetech.com>
2021-12-04 07:38:25 +0100

    Add JWT support for danubetech.

--- implementations/danubetech/src/main/java/ldsignatures.test.jws/JWSTestSuite.java
+++ implementations/danubetech/src/main/java/ldsignatures.test.jws/JWSTestSuite.java
@@ -4,9 +4,17 @@ import com.danubetech.keyformats.JWK_to_PrivateKey;
 import com.danubetech.keyformats.crypto.ByteSigner;
 import com.danubetech.keyformats.crypto.PrivateKeySignerFactory;
 import com.danubetech.keyformats.jose.JWK;
+import com.danubetech.keyformats.jose.JWSAlgorithm;
 import com.danubetech.keyformats.jose.KeyTypeName;
 import com.danubetech.keyformats.keytypes.KeyTypeName_for_JWK;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.danubetech.verifiablecredentials.VerifiableCredential;
+import com.danubetech.verifiablecredentials.VerifiablePresentation;
+import com.danubetech.verifiablecredentials.jsonld.VerifiableCredentialContexts;
+import com.danubetech.verifiablecredentials.jwt.JwtObject;
+import com.danubetech.verifiablecredentials.jwt.JwtVerifiableCredential;
+import com.danubetech.verifiablecredentials.jwt.JwtVerifiablePresentation;
+import com.danubetech.verifiablecredentials.jwt.ToJwtConverter;
+import com.nimbusds.jose.JOSEException;
 import foundation.identity.jsonld.JsonLDException;
 import foundation.identity.jsonld.JsonLDObject;
 import info.weboftrust.ldsignatures.jsonld.LDSecurityKeywords;
@@ -14,11 +22,9 @@ import info.weboftrust.ldsignatures.signer.JsonWebSignature2020LdSigner;
 import info.weboftrust.ldsignatures.suites.SignatureSuites;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.OpenOption;
-import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.Date;
@@ -26,59 +32,9 @@ import java.util.Map;
 
 public class JWSTestSuite {
 
-    static boolean isCredentialCreate(String[] args) {
-        return args != null && "credential".equals(args[0]) && "create".equals(args[1]);
-    }
-
-    static boolean isPresentationCreate(String[] args) {
-        return args != null && "presentation".equals(args[0]) && "create".equals(args[1]);
-    }
-
-    static String getParamValue(String[] args, String paramName) {
-        for (int i=0; i<args.length-1; i++) {
-            if (args[i].equals(paramName)) return args[i+1];
-        }
-        return null;
-    }
-
-    static String readFile(String path) throws IOException {
-        return path == null ? null : Files.readString(Paths.get(path));
-    }
-
-    static void writeFile(String path, String content) throws IOException {
-        Files.createDirectories(Paths.get(path).getParent());
-        Files.writeString(Paths.get(path), content, StandardCharsets.UTF_8);
-    }
-
-    static Map<String, Object> readKeyMap(String key) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> keyMap = objectMapper.readValue(key, Map.class);
-        if (keyMap == null) throw new IllegalArgumentException("No key map found: " + key);
-        return keyMap;
-    }
-
-    static URI readVerificationMethod(Map<String, Object> keyMap) throws IOException {
-        URI verificationMethod = keyMap == null ? null : URI.create((String) keyMap.get("id"));
-        if (verificationMethod == null) throw new IllegalArgumentException("No id found: " + keyMap);
-        return verificationMethod;
-    }
-
-    static JWK readJwk(Map<String, Object> keyMap, boolean privateKeyJwk) throws IOException {
-        Map<String, Object> jwkMap = keyMap == null ? null : (Map<String, Object>) keyMap.get(privateKeyJwk ? "privateKeyJwk" : "publicKeyJwk");
-        JWK keyJwk = jwkMap == null ? null : JWK.fromMap(jwkMap);
-        if (keyJwk == null) throw new IllegalArgumentException("No key found (" + privateKeyJwk + "): " + keyMap);
-        return keyJwk;
-    }
-
-    static void create(String input, String key, String outputFilename, String proofPurpose) throws JsonLDException, GeneralSecurityException, IOException {
+    static void create(String input, URI verificationMethod, ByteSigner byteSigner, String outputFilename, String proofPurpose) throws JsonLDException, GeneralSecurityException, IOException {
         JsonLDObject jsonLDObject = JsonLDObject.fromJson(input);
-        Map<String, Object> keyMap = readKeyMap(key);
-        URI verificationMethod = readVerificationMethod(keyMap);
-        JWK jwk = readJwk(keyMap, true);
-        KeyTypeName keyTypeName = KeyTypeName_for_JWK.keyTypeName_for_JWK(jwk);
-        String algorithm = SignatureSuites.SIGNATURE_SUITE_JSONWEBSIGNATURE2020.findDefaultJwsAlgorithmForKeyTypeName(keyTypeName);
-        Object privateKey = JWK_to_PrivateKey.JWK_to_anyPrivateKey(jwk);
-        ByteSigner byteSigner = PrivateKeySignerFactory.privateKeySignerForKey(keyTypeName, algorithm, privateKey);
+        jsonLDObject.setDocumentLoader(VerifiableCredentialContexts.DOCUMENT_LOADER);
         JsonWebSignature2020LdSigner jsonWebSignature2020LdSigner = new JsonWebSignature2020LdSigner();
         jsonWebSignature2020LdSigner.setVerificationMethod(verificationMethod);
         jsonWebSignature2020LdSigner.setCreated(new Date());
@@ -87,34 +43,83 @@ public class JWSTestSuite {
         jsonWebSignature2020LdSigner.setChallenge("123");
         jsonWebSignature2020LdSigner.sign(jsonLDObject, true, false);
         String output = jsonLDObject.toJson(true);
-        writeFile(outputFilename, output);
+        FileUtil.writeFile(outputFilename, output);
     }
 
-    static void credentialCreate(String input, String key, String outputFilename) throws JsonLDException, GeneralSecurityException, IOException {
-        create(input, key, outputFilename, LDSecurityKeywords.JSONLD_TERM_ASSERTIONMETHOD);
+    static void credentialCreate(String input, URI verificationMethod, ByteSigner byteSigner, String outputFilename) throws JsonLDException, GeneralSecurityException, IOException {
+        create(input, verificationMethod, byteSigner, outputFilename, LDSecurityKeywords.JSONLD_TERM_ASSERTIONMETHOD);
     }
 
-    static void presentationCreate(String input, String key, String outputFilename) throws JsonLDException, GeneralSecurityException, IOException {
-        create(input, key, outputFilename, LDSecurityKeywords.JSONLD_TERM_AUTHENTICATION);
+    static void presentationCreate(String input, URI verificationMethod, ByteSigner byteSigner, String outputFilename) throws JsonLDException, GeneralSecurityException, IOException {
+        create(input, verificationMethod, byteSigner, outputFilename, LDSecurityKeywords.JSONLD_TERM_AUTHENTICATION);
+    }
+
+    static void createJwt(JwtObject jwtObject, URI verificationMethod, ByteSigner byteSigner, String outputFilename) throws JOSEException, IOException {
+        String jwtString;
+        if (JWSAlgorithm.EdDSA.equals(byteSigner.getAlgorithm()))
+            jwtString = jwtObject.sign_Ed25519_EdDSA(byteSigner, verificationMethod.toString(), false);
+        else if (JWSAlgorithm.ES256K.equals(byteSigner.getAlgorithm()))
+            jwtString = jwtObject.sign_secp256k1_ES256K(byteSigner, verificationMethod.toString(), false);
+        else if (JWSAlgorithm.PS256.equals(byteSigner.getAlgorithm()))
+            jwtString = jwtObject.sign_RSA_RS256(byteSigner, verificationMethod.toString(), false);
+        else
+            throw new IllegalArgumentException("Unsupported algorithm: " + byteSigner.getAlgorithm());
+        String output = "{\"jwt\":\"" + jwtString + "\"}";
+        FileUtil.writeFile(outputFilename, output);
+    }
+
+    static void credentialCreateJwt(String input, URI verificationMethod, ByteSigner byteSigner, String outputFilename) throws JOSEException, IOException {
+        VerifiableCredential verifiableCredential = VerifiableCredential.fromJson(input);
+        JwtVerifiableCredential jwtVerifiableCredential = ToJwtConverter.toJwtVerifiableCredential(verifiableCredential);
+        createJwt(jwtVerifiableCredential, verificationMethod, byteSigner, outputFilename);
+    }
+
+    static void presentationCreateJwt(String input, URI verificationMethod, ByteSigner byteSigner, String outputFilename) throws JOSEException, IOException {
+        VerifiablePresentation verifiablePresentation = VerifiablePresentation.fromJson(input);
+        JwtVerifiablePresentation jwtVerifiablePresentation = ToJwtConverter.toJwtVerifiablePresentation(verifiablePresentation);
+        createJwt(jwtVerifiablePresentation, verificationMethod, byteSigner, outputFilename);
     }
 
     public static void main(String[] args) throws Throwable {
 
-        String inputFilename = getParamValue(args, "--input");
-        String outputFilename = getParamValue(args, "--output");
-        String keyFilename = getParamValue(args, "--key");
+        System.out.println(Arrays.asList(args));
+
+        String inputFilename = ParamUtil.getParamValue(args, "--input");
+        String outputFilename = ParamUtil.getParamValue(args, "--output");
+        String keyFilename = ParamUtil.getParamValue(args, "--key");
+        String format = ParamUtil.getParamValue(args, "--format");
         if (inputFilename == null) throw new IllegalArgumentException("No input filename.");
         if (outputFilename == null) throw new IllegalArgumentException("No output filename.");
         if (keyFilename == null) throw new IllegalArgumentException("No key filename.");
+        if (format == null) throw new IllegalArgumentException("No format.");
 
-        String input = readFile(inputFilename);
-        String key = readFile(keyFilename);
+        String input = FileUtil.readFile(inputFilename);
+        String key = FileUtil.readFile(keyFilename);
 
-        if (isCredentialCreate(args))
-            credentialCreate(input, key, outputFilename);
-        else if (isPresentationCreate(args))
-            presentationCreate(input, key, outputFilename);
-        else
-            throw new IllegalArgumentException("Invalid request: " + Arrays.asList(args));
+        Map<String, Object> keyMap = KeyUtil.readKeyMap(key);
+        URI verificationMethod = KeyUtil.readVerificationMethod(keyMap);
+        JWK jwk = KeyUtil.readJwk(keyMap, true);
+        KeyTypeName keyTypeName = KeyTypeName_for_JWK.keyTypeName_for_JWK(jwk);
+        String algorithm = SignatureSuites.SIGNATURE_SUITE_JSONWEBSIGNATURE2020.findDefaultJwsAlgorithmForKeyTypeName(keyTypeName);
+        Object privateKey = JWK_to_PrivateKey.JWK_to_anyPrivateKey(jwk);
+        ByteSigner byteSigner = PrivateKeySignerFactory.privateKeySignerForKey(keyTypeName, algorithm, privateKey);
+
+        try {
+            if (ParamUtil.isCredentialCreate(args, format))
+                credentialCreate(input, verificationMethod, byteSigner, outputFilename);
+            else if (ParamUtil.isPresentationCreate(args, format))
+                presentationCreate(input, verificationMethod, byteSigner, outputFilename);
+            else if (ParamUtil.isCredentialCreateJwt(args, format))
+                credentialCreateJwt(input, verificationMethod, byteSigner, outputFilename);
+            else if (ParamUtil.isPresentationCreateJwt(args, format))
+                presentationCreateJwt(input, verificationMethod, byteSigner, outputFilename);
+            else
+                throw new IllegalArgumentException("Invalid request: " + Arrays.asList(args));
+        } catch (Exception ex) {
+            StringWriter output = new StringWriter();
+            ex.printStackTrace(new PrintWriter(output));
+            FileUtil.writeFile(outputFilename, output.toString());
+            throw ex;
+        }
     }
 }
